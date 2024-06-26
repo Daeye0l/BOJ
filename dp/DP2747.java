08b747d616fdef540a7cb293b5fc76f900faee91
Daeyeol Sung <tjdeoduf1228@gmail.com>
2024-06-26 16:57:01 +0900

    2747

---
+++ dp/DP2747.java
@@ -0,0 +1,21 @@
+package dp;
+
+import java.io.*;
+
+public class DP2747 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[46];
+		dp[0] = 0;
+		dp[1] = 1;
+		
+		for(int i = 2; i < 46; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		
+		System.out.println(dp[n]);
+	}
+}
