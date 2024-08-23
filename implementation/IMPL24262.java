923d4ebb24ca242d3f064d7279b16d93038b1c3b
Daeyeol Sung <tjdeoduf1228@gmail.com>
2024-08-23 17:58:21 +0900

    24262

---
+++ implementation/IMPL24262.java
@@ -0,0 +1,12 @@
+package implementation;
+
+import java.io.*;
+
+public class IMPL24262 {
+
+	public static void main(String[] args) throws IOException {
+		// MenOfPassion 알고리즘은 1번 시행 되고, 상수항의 차수 0 출력
+		System.out.println(1);
+		System.out.println(0);
+	}
+}
